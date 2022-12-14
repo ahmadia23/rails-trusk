ec86ba8d44b21bb3b83a01fa3f81f295174f2828
ahmadou dia <dia.ahmadou23@gmail.com>
2022-12-14 22:29:28 +0100

    started show page and designes card product in index and card in show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,20 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+
+  private
+
+  def set_params
+    params.require(:courses).permit(:address, :starting_time, :ending_time, :price, :client_name, :pro_client)
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
 end
